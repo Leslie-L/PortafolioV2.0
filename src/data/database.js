9691f6781450cc92c8fb9741a2ebbb3ef241aa24
Leslie-L <leslie.centeno@galileo.edu>
2024-04-02 17:33:06 -0600

    databes and tools on skills

--- src/data/database.js
+++ src/data/database.js
@@ -1,17 +1,7 @@
-import Postgres from "../components/icons/postgres.astro"
-import Firebase from "../components/icons/firebase.astro"
-import Sql from "../components/icons/sql.astro"
+
 export const database =[
-    {
-        name:'SQL',
-        icon: Sql
-    },
-    {
-        name:'Postgres',
-        icon: Postgres
-    },
-    {
-        name:'Firebase',
-        icon: Firebase
-    },
+    'SQL',
+    'Postgres',
+     'Firebase',
+        
 ]
--- src/data/tools.js
+++ src/data/tools.js
@@ -1,18 +1,7 @@
-import Git from "../components/icons/git.astro"
-import Github from "../components/icons/github.astro"
-import Figma from "../components/icons/figma.astro"
 
 export const tools=[
-    {
-        name:'Git',
-        icon: Git
-    },
-    {
-        name:'Github',
-        icon: Github
-    },
-    {
-        name:'Figma',
-        icon: Figma
-    },
+    'Git',
+    'Github',
+     'Figma',
+      
 ]
