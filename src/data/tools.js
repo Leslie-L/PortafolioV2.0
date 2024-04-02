515f4a38a17822f07d1a92c76dc1b693e5d854c1
Leslie-L <leslie.centeno@galileo.edu>
2024-04-02 16:09:30 -0600

    tools icons

---
+++ src/data/tools.js
@@ -0,0 +1,18 @@
+import Git from "../components/icons/git.astro"
+import Github from "../components/icons/github.astro"
+import Figma from "../components/icons/figma.astro"
+
+export const tools=[
+    {
+        name:'Git',
+        icon: Git
+    },
+    {
+        name:'Github',
+        icon: Github
+    },
+    {
+        name:'Figma',
+        icon: Figma
+    },
+]
