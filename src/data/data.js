53409fcc629c3a15a20abbdadcc9d63a01d0a34b
Leslie-L <leslie.centeno@galileo.edu>
2024-04-02 15:09:56 -0600

    data and icons

---
+++ src/data/data.js
@@ -0,0 +1,23 @@
+import R from "../components/icons/r.astro"
+import Pandas from "../components/icons/pandas.astro"
+import Numpy from "../components/icons/numpy.astro"
+import Scikitlearn from "../components/icons/scikitlearn.astro"
+
+export const data = [
+    {
+        name:'R',
+        icon: R
+    },
+    {
+        name:'Pandas',
+        icon: Pandas
+    },
+    {
+        name:'Numpy',
+        icon: Numpy
+    },
+    {
+        name:'Scikitlearn',
+        icon: Scikitlearn
+    },
+]
