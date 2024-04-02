a7bdbad9deacb299eb3a60dd6bc793a239414639
Leslie-L <leslie.centeno@galileo.edu>
2024-04-02 15:01:33 -0600

    backend icons

---
+++ src/data/backend.js
@@ -0,0 +1,23 @@
+import Node from "../components/icons/node.astro"
+import Python from "../components/icons/python.astro"
+import Php from "../components/icons/php.astro"
+import Java from "../components/icons/java.astro"
+
+export const backend = [
+    {
+        name:'Node.js',
+        icon: Node
+    },
+    {
+        name:'Python',
+        icon: Python
+    },
+    {
+        name:'PHP',
+        icon: Php
+    },
+    {
+        name:'Java',
+        icon: Java
+    },
+]
