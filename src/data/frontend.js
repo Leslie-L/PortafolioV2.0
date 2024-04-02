17c9d54b66477dd24c070078fb0c52e3c020ee67
Leslie-L <leslie.centeno@galileo.edu>
2024-04-02 14:22:10 -0600

    skills file

---
+++ src/data/frontend.js
@@ -0,0 +1,43 @@
+import Boostrap from "../components/icons/boostrap.astro"
+import Css from "../components/icons/css.astro"
+import Html from "../components/icons/html.astro"
+import Javascript from "../components/icons/javascript.astro"
+import React from "../components/icons/react.astro"
+import Tailwind from "../components/icons/tailwind.astro"
+import Next from "../components/icons/next.astro"
+import Typescript from "../components/icons/typescript.astro"
+export const fronend = [
+    {
+        name:'HTML',
+        icon: Html
+    },
+    {
+        name:'CSS',
+        icon:Css
+    },
+    {
+        name:'Boostrap',
+        icon:Boostrap
+    },
+    {
+        name:'Tailwind',
+        icon:Tailwind
+    },
+    {
+        name:'Javascript',
+        icon:Javascript
+    },
+    {
+        name:'Typescript',
+        icon:Typescript
+    },
+    {
+        name:'React',
+        icon:React
+    },
+    {
+        name:'Next.js',
+        icon:Next
+    },
+
+]
