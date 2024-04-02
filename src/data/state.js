469abd56c2d333c4886f2f600653d29344ec4cf0
Leslie-L <leslie.centeno@galileo.edu>
2024-04-02 14:58:27 -0600

    state managment icons

---
+++ src/data/state.js
@@ -0,0 +1,12 @@
+import Redux from "../components/icons/redux.astro"
+import Zustand from "../components/icons/zustand.astro"
+export const store = [
+    {
+        name:'Redux',
+        icon: Redux
+    },
+    {
+        name:'Zustand',
+        icon: Zustand
+    }
+]
