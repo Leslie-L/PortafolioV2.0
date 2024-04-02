c1b3f4d722ae8c9a2d5992c39901b57a64fdc286
Leslie-L <leslie.centeno@galileo.edu>
2024-04-02 17:08:44 -0600

    frontend and state on skills section

--- src/data/frontend.js
+++ src/data/frontend.js
@@ -1,43 +1,11 @@
-import Boostrap from "../components/icons/boostrap.astro"
-import Css from "../components/icons/css.astro"
-import Html from "../components/icons/html.astro"
-import Javascript from "../components/icons/javascript.astro"
-import React from "../components/icons/react.astro"
-import Tailwind from "../components/icons/tailwind.astro"
-import Next from "../components/icons/next.astro"
-import Typescript from "../components/icons/typescript.astro"
-export const fronend = [
-    {
-        name:'HTML',
-        icon: Html
-    },
-    {
-        name:'CSS',
-        icon:Css
-    },
-    {
-        name:'Boostrap',
-        icon:Boostrap
-    },
-    {
-        name:'Tailwind',
-        icon:Tailwind
-    },
-    {
-        name:'Javascript',
-        icon:Javascript
-    },
-    {
-        name:'Typescript',
-        icon:Typescript
-    },
-    {
-        name:'React',
-        icon:React
-    },
-    {
-        name:'Next.js',
-        icon:Next
-    },
 
+export const frontend = [
+    'HTML',
+    'CSS',
+    'Boostrap',
+    'Tailwind',
+	'Javascript',
+    'Typescript',
+	'React',
+	'Next',
 ]
--- src/data/state.js
+++ src/data/state.js
@@ -1,12 +1,5 @@
-import Redux from "../components/icons/redux.astro"
-import Zustand from "../components/icons/zustand.astro"
-export const store = [
-    {
-        name:'Redux',
-        icon: Redux
-    },
-    {
-        name:'Zustand',
-        icon: Zustand
-    }
+
+export const state = [
+    'Redux',
+    'Zustand',    
 ]
