b4b821a74c09f59e76d8daa5bf385182e9d5333d
Leslie-L <leslie.centeno@galileo.edu>
2024-04-02 16:01:59 -0600

    testing icons

---
+++ src/data/testing.js
@@ -0,0 +1,33 @@
+import Jest from "../components/icons/jest.astro"
+import Playwright from "../components/icons/playwright.astro"
+import Puppeteer from "../components/icons/puppeteer.astro"
+import Cypress from "../components/icons/cypress.astro"
+import Postman from "../components/icons/postman.astro"
+import Selenium from "../components/icons/selenium.astro"
+
+export const testing = [
+    {
+        name:'Jest',
+        icon: Jest
+    },
+    {
+        name:'Playwright',
+        icon: Playwright
+    },
+    {
+        name:'Puppeteer',
+        icon: Puppeteer
+    },
+    {
+        name:'Cypress',
+        icon: Cypress
+    },
+    {
+        name:'Postman',
+        icon: Postman
+    },
+    {
+        name:'Selenium',
+        icon: Selenium
+    }
+]
