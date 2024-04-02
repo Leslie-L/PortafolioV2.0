225d69899c34f5b444515d78b2bbd0b9e3052841
Leslie-L <leslie.centeno@galileo.edu>
2024-04-02 17:17:36 -0600

    backend and data science on skills

--- src/data/data.js
+++ src/data/data.js
@@ -4,20 +4,9 @@ import Numpy from "../components/icons/numpy.astro"
 import Scikitlearn from "../components/icons/scikitlearn.astro"
 
 export const data = [
-    {
-        name:'R',
-        icon: R
-    },
-    {
-        name:'Pandas',
-        icon: Pandas
-    },
-    {
-        name:'Numpy',
-        icon: Numpy
-    },
-    {
-        name:'Scikitlearn',
-        icon: Scikitlearn
-    },
+    'R',
+    'Pandas',
+    'Numpy',
+     'Scikitlearn',
+      
 ]
