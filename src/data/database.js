8666c908dc375771007ee3cc675e2645a6ec9d37
Leslie-L <leslie.centeno@galileo.edu>
2024-04-02 16:07:21 -0600

    databases icons

---
+++ src/data/database.js
@@ -0,0 +1,17 @@
+import Postgres from "../components/icons/postgres.astro"
+import Firebase from "../components/icons/firebase.astro"
+import Sql from "../components/icons/sql.astro"
+export const database =[
+    {
+        name:'SQL',
+        icon: Sql
+    },
+    {
+        name:'Postgres',
+        icon: Postgres
+    },
+    {
+        name:'Firebase',
+        icon: Firebase
+    },
+]
